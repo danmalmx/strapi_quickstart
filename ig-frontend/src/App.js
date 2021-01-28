c8f28e79d267d156e07049494757891b7000b419
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-01-28 21:08:38 +0100

    Render multiple post using map

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Post from './components/Post'
 
+const posts = [
+  {
+    likes: 10,
+    description: 'Just plain old me. Well, not just!!',
+    image: {
+      url: '/uploads/daniel_malmgren_OLA_7478_a6d556674e.jpg',
+    }
+  },
+  {
+    likes: 30,
+    description: 'Second post',
+    image: {
+      url: '/uploads/daniel_malmgren_OLA_7478_a6d556674e.jpg',
+    }
+  },
+]
+
+
 function App() {
   return (
     <div className="App">
-      <Post />
+      {posts.map(post => (
+        <Post
+          likes={post.likes}
+          description={post.description}
+          url={post.image && post.image.url}
+        />
+      ))}
     </div>
   );
 }
--- ig-frontend/src/components/Post.js
+++ ig-frontend/src/components/Post.js
@@ -9,39 +9,15 @@ const post = {
 	"created_at": "2021-01-27T19:04:47.071Z",
 	"updated_at": "2021-01-27T19:04:54.966Z",
 	"image": {
-			"id": 1,
-			"name": "daniel_malmgren_OLA7478.jpg",
-			"alternativeText": "",
-			"caption": "",
-			"width": 140,
-			"height": 140,
-			"formats": null,
-			"hash": "daniel_malmgren_OLA_7478_a6d556674e",
-			"ext": ".jpg",
-			"mime": "image/jpeg",
-			"size": 4.85,
 			"url": "/uploads/daniel_malmgren_OLA_7478_a6d556674e.jpg",
-			"previewUrl": null,
-			"provider": "local",
-			"provider_metadata": null,
-			"created_at": "2021-01-27T19:04:19.985Z",
-			"updated_at": "2021-01-27T19:04:20.002Z"
 	}
 }
 
 const API_URL = 'http://localhost:1337'
 const formatImageUrl = (url) => `${API_URL}${url}`
 
-const url = post.image && post.image.url;
-console.log('url', url);
-const description = post.description;
-console.log('description', description);
-const likes = post.likes;
-console.log('likes', likes);
+export default ({ description, likes, url }) => {
 
-
-
-export default () => {
 	return (
 		<div className="Post">
 			<img className="Post__Image" src={formatImageUrl(url)} alt=""/>
