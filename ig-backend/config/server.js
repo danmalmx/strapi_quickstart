7a6d72f3ccfb5c6309727bfa716bd2bc0f0ab175
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2021-01-28 19:08:52 +0100

    Set up strapi and react

---
+++ ig-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3032a99699bdda7b7cdee4881329e0f3'),
+    },
+  },
+});
---
+++ ig-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '279d4719-c3e5-44f2-a608-ac3b26953a77'
+};
